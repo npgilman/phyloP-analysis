be6cce921c4956e5f8064ce18545cfb7a31b093a
Nathan Gilman <ngilman@ufl.edu>
2024-10-23 16:12:26 -0400

    Filter last column to gene id + synonyms. Drop columns of '.'

--- format_output.py
+++ format_output.py
@@ -4,13 +4,13 @@ import pandas as pd
 
 def format(mapped_data, phyloP_df, gtf_df, output_path):
     output_data = next(iter(output_path))
-    
-    header = '#chr\tstart\tend\tname\tnull_scale\talt_scale\talt_subscale\tlnlratio\tpval\n'
-    df_header = header.split('\t')
-    df_header[-1] = 'pval'
 
-    new_columns = phyloP_df.columns.tolist() + df_header
+    # gtf_header = ["0", "1", "2", "3", "4", "5", "6", "7", "8"]
+    new_columns = phyloP_df.columns.tolist() + gtf_df.columns.tolist()
     resultDf = pd.DataFrame(columns = new_columns)
+
+    gtf_df.iloc[:,8] = gtf_df.iloc[:,8].apply(lambda x : x.split(';')).apply(lambda x : [y.split() for y in x])
+    gtf_df.iloc[:,8] = gtf_df.iloc[:,8].apply(lambda data : [item for item in data if item and (item[0] == 'gene_id' or item[0] == 'gene_synonym')])
 
     for key, arr in mapped_data.items():
         gtf_row = gtf_df.loc[key].tolist()
@@ -19,5 +19,6 @@ def format(mapped_data, phyloP_df, gtf_df, output_path):
             result = phyloP_row + gtf_row
             resultDf.loc[i] = result
 
+    resultDf = resultDf.drop(columns=['5', '7'])
 
     resultDf.to_csv(output_path, sep='\t', index=False)
